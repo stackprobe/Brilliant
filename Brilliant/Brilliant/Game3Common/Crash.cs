9497cb215f155cde9f3710b9fb8b258ad8c4c53c
stackprobe <stackprobes@gmail.com>
2019-09-13 08:56:08 +0900

    Rose

---
+++ Brilliant/Brilliant/Game3Common/Crash.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Tools;
+
+namespace Charlotte.Game3Common
+{
+	public class Crash
+	{
+		// インスタンスの生成は CrashUtils で、
+
+		public enum Kind_e
+		{
+			NONE = 1,
+			POINT,
+			CIRCLE,
+			RECT,
+		}
+
+		public Kind_e Kind = Kind_e.POINT;
+		public D2Point Pt = new D2Point(-1, 0);
+		public double R = 0.0;
+		public D4Rect Rect = null;
+
+		public bool IsCrashed(Crash other)
+		{
+			return CrashUtils.IsCrashed(this, other);
+		}
+	}
+}
