851cd63d5c5b82bd996c77a4c27e8bddb79bda1d
stackprobe <stackprobes@gmail.com>
2019-09-24 03:42:42 +0900

    Earl

--- Brilliant/Brilliant/Program2.cs
+++ Brilliant/Brilliant/Program2.cs
@@ -6,6 +6,7 @@ using DxLibDLL;
 using Charlotte.Common;
 using Charlotte.Tools;
 using Charlotte.Tests;
+using Charlotte.Tests.Game3Common;
 
 namespace Charlotte
 {
@@ -66,7 +67,8 @@ namespace Charlotte
 
 		private void Main4()
 		{
-			new Test0001().Test01();
+			//new Test0001().Test01();
+			new CResourceTest().Test01();
 		}
 	}
 }
---
+++ Brilliant/Brilliant/Tests/Game3Common/CResourceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Game3Common;
+using Charlotte.Common;
+
+namespace Charlotte.Tests.Game3Common
+{
+	public class CResourceTest
+	{
+		public void Test01()
+		{
+			for (; ; )
+			{
+				DDCurtain.DrawCurtain();
+
+				DDDraw.DrawSimple(CResource.GetPicture(@"CResource\Picture\Picture0001.png"), 0, 0);
+				DDDraw.DrawSimple(CResource.GetPicture(@"CResource\Picture\Picture0002.png"), 200, 0);
+				DDDraw.DrawSimple(CResource.GetPicture(@"CResource\Picture\Picture0003.png"), 400, 0);
+
+				DDEngine.EachFrame();
+			}
+		}
+	}
+}
