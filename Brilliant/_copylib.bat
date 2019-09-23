83dca6f18efc66868b8dbb5a61ec554068361045
stackprobe <stackprobes@gmail.com>
2019-09-24 02:43:18 +0900

    Pine

--- Brilliant/_copylib.bat
+++ Brilliant/_copylib.bat
@@ -1,2 +1,3 @@
 C:\Factory\SubTools\CopyLib.exe C:\Dev\Fairy\Donut2\Donut2\Donut2\Common Brilliant\Common
+C:\Factory\SubTools\CsProjAdjustCs.exe Brilliant Common
 ROBOCOPY C:\Dev\Fairy\Donut2\Resource\General ..\Resource\General /MIR
